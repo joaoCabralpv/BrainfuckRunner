28d4ef98cbfac12e84b762bc166a9f163d743293
João Cabral <joaocabralpv@gmail.com>
2022-12-27 16:07:42 -0100

    [ and ]

--- bf.py
+++ bf.py
@@ -1,32 +1,60 @@
+#initializes the array with zeros
+code = """
+a=[]
+for i in range(30000):
+    a.append(0)
+          
+i=0
+
+"""
+#opens the files 
 file = open("r.py","w")
 bfcode = open("test.bf", "r")
 bfcode = bfcode.read()
-bfcode = bfcode.replace(" ","").replace("\n","").replace("\t","")
+
+TabCounter = 0
+LoopCounter = 0
+LoopCommandArray = []
 
 
-file.write("""a=[]
-for i in range(30000):
-    a.append(0)
-              
-i=0
-""")
 
 for i in range(len(bfcode)):
+    NewLine = True
     if bfcode[i]=="+":
-        file.write("a[i]+=1")
+        code+="\t"*TabCounter+"a[i]+=1 "#+"
+
     elif bfcode[i]=="-":
-        file.write("a[i]-=1")
+        code+="\t"*TabCounter+"a[i]-=1 "#-"
+
     elif bfcode[i]=="<":
-        file.write("""try:
-    i-=1
-    a[i]
-except:
-    i = 30000""")
+        code+="\t"*TabCounter+"i = i-1 if i-1>0 else 30000-1 "#\n"+"\t"*TabCounter+"except:"+"i=30000 "#<"
+
     elif bfcode[i]==">":
-        file.write("""try:
-    i+=1
-    a[i]
-except:
-    i=0""")
-    file.write("\n")
-    
+        code+="\t"*TabCounter+"i=i+1 if i+1>30000 else 0"# \n"+"\t"*TabCounter+"except:"+"i=0 "#>"
+
+    elif bfcode[i]==".":
+        code+="\t"*TabCounter+"print(chr(a[i]),end='') "#."
+
+    elif bfcode[i]==",":
+        code+="\t"*TabCounter+"a[i]=ord(input()) "#,"
+
+    elif bfcode[i]=="[":
+        LoopCounter+=1
+        LoopCommandArray.append(i)
+        code+="\t"*TabCounter+("b"*LoopCounter)+" = not a[i]\n"
+        code+="\t"*TabCounter+"while a[i]: "#["
+
+        TabCounter+=1
+    elif bfcode[i]=="]":
+        #code+="#]\n"
+        TabCounter-=1
+        LoopCounter-=1
+    else:
+        NewLine = False
+    if NewLine:
+        code+="\n"
+
+file.write(code)
+
+
+
